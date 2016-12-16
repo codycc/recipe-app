c5559a054d1753774d21e68a99910107a1fc54b7
Cody Condon <codycondon50@gmail.com>
2016-12-15 20:19:00 -0500

    small change to scss file

--- app/app.module.js
+++ app/app.module.js
@@ -1,1 +1,31 @@
-"use strict";var __decorate=this&&this.__decorate||function(e,t,r,o){var n,p=arguments.length,i=p<3?t:null===o?o=Object.getOwnPropertyDescriptor(t,r):o;if("object"==typeof Reflect&&"function"==typeof Reflect.decorate)i=Reflect.decorate(e,t,r,o);else for(var c=e.length-1;c>=0;c--)(n=e[c])&&(i=(p<3?n(i):p>3?n(t,r,i):n(t,r))||i);return p>3&&i&&Object.defineProperty(t,r,i),i},core_1=require("@angular/core"),platform_browser_1=require("@angular/platform-browser"),http_1=require("@angular/http"),app_component_1=require("./app.component"),ingredient_filter_pipe_1=require("./pipes/ingredient-filter.pipe"),ingredient_selector_1=require("./ingredient-selector/ingredient-selector"),AppModule=function(){function e(){}return e}();AppModule=__decorate([core_1.NgModule({imports:[platform_browser_1.BrowserModule,http_1.HttpModule],declarations:[app_component_1.AppComponent,ingredient_filter_pipe_1.IngredientFilter,ingredient_selector_1.IngredientSelector],bootstrap:[app_component_1.AppComponent]})],AppModule),exports.AppModule=AppModule;
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
+var platform_browser_1 = require('@angular/platform-browser');
+var http_1 = require('@angular/http');
+var app_component_1 = require('./app.component');
+var ingredient_filter_pipe_1 = require('./pipes/ingredient-filter.pipe');
+var ingredient_selector_1 = require("./ingredient-selector/ingredient-selector");
+var AppModule = (function () {
+    function AppModule() {
+    }
+    AppModule = __decorate([
+        core_1.NgModule({
+            imports: [platform_browser_1.BrowserModule, http_1.HttpModule],
+            declarations: [app_component_1.AppComponent, ingredient_filter_pipe_1.IngredientFilter, ingredient_selector_1.IngredientSelector],
+            bootstrap: [app_component_1.AppComponent]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppModule);
+    return AppModule;
+}());
+exports.AppModule = AppModule;
+//# sourceMappingURL=app.module.js.map
